08bc185d46a7a9974bc5f0d349a4051627b1680e
Ganti Sai Sagar <sagarkr1109@gmail.com>
2021-10-24 02:01:20 +0530

    done

---
+++ src/com/company/ntermofSeries.java
@@ -0,0 +1,10 @@
+class Solution {
+    static int findNthTerm(int N) {
+        // code here
+        int sum = 0;
+        for(int i = 0;i<=N;i++){
+            sum+=i;
+        }
+        return sum;
+    }
+}
